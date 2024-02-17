29ad39626062753f9cc76778d6f784a220e826a1
Mikayla <149916992+mikaylaakelly@users.noreply.github.com>
2024-02-16 20:20:27 -0500

    adding the framework for the functions I need

--- db/schema.sql
+++ db/schema.sql
@@ -10,22 +10,19 @@ CREATE TABLE department (
 CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
     department_id INT,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
-    ON DELETE SET NULl
+    ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    emp_id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    roles_id INT,
-    FOREIGN KEY (roles_id)
-    REFERENCES roles(id), 
-    manager_id INT,
-    FOREIGN KEY (manager_id)
-    REFERENCES employee(id)
-    ON DELETE SET NULL
+    roles_id INTEGER,
+    manager_id INTEGER,
+    FOREIGN KEY (roles_id) REFERENCES roles(id) ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES employee(emp_id) ON DELETE SET NULL
 );
--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const mysql = require('mysql2');
+const dotenv = require('dotenv');
+const inquirer = require('inquirer');
+
+
+dotenv.config();
 
 const PORT = process.env.PORT || 3306;
 const app = express();
@@ -20,3 +25,61 @@ const db = mysql.createConnection(
     console.log(`connected to the company_db database.`)
   );
   
+  function employeeTracker(){
+    inquirer.prompt([
+      {
+        type: 'list',
+        name: 'viewChoices',
+        choices: ['View all Departments', 'View all Roles', 'View all Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role' ]
+      }
+    ]).then(answer => {
+      switch (answer.viewChoices) {
+        case 'View all Departments':
+          console.log('Viewing Departments!');
+          viewDepartments();
+          break;
+  
+        case 'View all Roles':
+          console.log('Viewing Roles!');
+          viewAllRoles();
+          break;
+  
+        case 'View all Employees':
+          console.log('Viewing Employees!');
+          viewAllEmployees();
+          break;
+  
+        case 'Add a Department':
+          console.log('Adding a Department!');
+          addDepartment();
+          break;
+  
+        case 'Add a Role':
+          console.log('Adding a Role!');
+          addRole();
+          break;
+  
+        case 'Add an Employee':
+          console.log('Adding an Employee!');
+          addEmployee();
+          break;
+  
+        case 'Update an Employee Role':
+          console.log('Updating an Employee Role!');
+          updateEmployeeRole();
+          break;
+  
+        case 'End':
+          console.log('Exiting application.');
+          db.end();
+          break;
+      }
+    });
+  }
+
+
+
+
+app.listen(PORT, () => {
+console.log(`Server running on port ${PORT}`);
+});
